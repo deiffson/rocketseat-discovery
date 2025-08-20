a95338a776446c0ea1a6e054ec302200820b4d2b
deiffson <deiffsonadriel1999@gmail.com>
2025-08-20 14:15:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickSwitch() {
+  const html = document.documentElement
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatardeiflight.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
